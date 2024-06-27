9272694a80a1f733e549ebfab77fdf8df4a44dfe
Jules <jules.gresset@epitech.eu>
2024-06-27 17:23:24 +0200

    feat: page orders avec le nouceau component pour afficher les commandes

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,33 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
 import Order from '../components/Order';
+import './Orders.css';
 
 function Orders() {
 
     const [ordersList, setOrdersList] = useState([]);
   
     useEffect(() => {
-      async function fetchData() {
-        fetch ("http://localhost:4000/api/orders/").then(response => {
-          response.json().then(data => {
-            setOrdersList(data.map(order => <Order id={order.id}/>));
-          });
-        }).catch(error => {
-          console.log(error);
+      fetch ("http://localhost:4000/api/orders/").then(response => {
+        response.json().then(data => {
+          setOrdersList(data.map(order => <Order id={order.id}/>));
         });
-      };
-  
-      fetchData();
+      }).catch(error => {
+        console.log(error);
+      });
     }, []);
-  
+
     return (
       <div>
-        <header>
-          <h1>Orders</h1>
-          <ul>
-            {ordersList}
-          </ul>
-        </header>
+        <h1>Orders</h1>
+        <div class="grid grid-cols-5 gap-4 mx-2">
+          {ordersList}
+        </div>
       </div>
     );
   }
