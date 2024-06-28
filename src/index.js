fb0e1933c357765cc95605a8cf7889ca268d22e3
Lucas_lgd02 <lucas.guiard@epitech.eu>
2024-06-28 14:06:00 +0200

    fix: Ajout tailwindcss dans la page login

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Login from './pages/Login';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
