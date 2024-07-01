50bf1f46b19ac68960b60d60ceadf2a6081d8113
Jules <jules.gresset@epitech.eu>
2024-07-01 14:21:35 +0200

    feat: Affichage dynamique des commandes

--- src/Components/OrdersDisplay/OrdersDisplay.js
+++ src/Components/OrdersDisplay/OrdersDisplay.js
@@ -1,29 +1,40 @@
-import React, { useState, useEffect } from 'react';
-
-import SingleOrderDisplay from './SingleOrderDisplay';
-import './Orders.css';
+import React, { useState, useEffect } from "react";
+import SingleOrderDisplay from "./SingleOrderDisplay";
+import "./Orders.css";
 
 function OrdersDisplay() {
+  const [ordersList, setOrdersList] = useState([]);
 
-    const [ordersList, setOrdersList] = useState([]);
-
-    useEffect(() => {
-      fetch (`http://${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/orders/`).then(response => {
-        response.json().then(data => {
-          setOrdersList(data.map(order => <SingleOrderDisplay key={order.id} id={order.id}/>));
-        });
-      }).catch(error => {
-        console.log(error);
+  const fetchOrders = () => {
+    fetch(
+      `http://${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/orders/`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setOrdersList(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching orders:", error);
       });
-    }, []);
+  };
 
-    return (
-      <div>
-        <div className="grid grid-cols-5 gap-4 mx-2 py-2">
-          {ordersList}
-        </div>
+  useEffect(() => {
+    fetchOrders();
+
+    const intervalId = setInterval(fetchOrders, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      <div className="grid grid-cols-5 gap-4 mx-2 py-2">
+        {ordersList.map((order) => (
+          <SingleOrderDisplay key={order.id} id={order.id} />
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default OrdersDisplay;
