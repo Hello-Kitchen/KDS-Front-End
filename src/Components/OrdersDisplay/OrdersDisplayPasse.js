74da1e6e26516b1156f9b2b98e555eb06723aead
Jules <jules.gresset@epitech.eu>
2024-10-08 18:12:51 -0400

    refactor: passe orders in compliance with new back

--- src/Components/OrdersDisplay/OrdersDisplayPasse.js
+++ src/Components/OrdersDisplay/OrdersDisplayPasse.js
@@ -12,12 +12,12 @@ function OrdersDisplayPasse({ status }) {
       let nbrLines = 0;
       let nbrCol = 0;
 
-      orderDetails.food.forEach((food) => {
+      orderDetails.food_ordered.map((food) => {
         nbrLines += 1;
-        food.details.forEach(() => {
+        food.details.map(() => {
           nbrLines += 1;
         });
-        food.mods_ingredients.forEach(() => {
+        food.mods_ingredients.map(() => {
           nbrLines += 1;
         });
         if (food.note) {
@@ -28,37 +28,66 @@ function OrdersDisplayPasse({ status }) {
       return nbrCol;
     };
 
-    fetch(`http://${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/orders?status=${status}&sort=time`)
+    fetch(
+      `http://${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/${process.env.REACT_APP_NBR_RESTAURANT}/orders?sort=time`
+    )
       .then((response) => response.json())
-      .then((data) => {
-        setNbrOrders(data.length);
+      .then((ordersData) => {
+        
+        
+        // Filter orders to display, only those that have at least one food that is not ready
+        
+        const orderToDisplay = [];
+        
+        ordersData.forEach((order) => {
+          const foodPart = [];
+          order.food_ordered.forEach((food) => {
+            if (food.part === order.part) {
+              foodPart.push(food);
+            }
+          });
+          if (status == "ready" && foodPart.every((food) => food.is_ready)) {
+            orderToDisplay.push(order);
+          } else if (status == "pending" && foodPart.some((food) => food.is_ready) && !foodPart.every((food) => food.is_ready)) {
+            orderToDisplay.push(order);
+          }
+        });
 
-        const fetchOrderDetailsPromises = data.slice(0, 10).map((order) => {
-          return fetch(`http://${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/orders/${order.id}?forKDS=true`)
+        // Used to display the number of orders waiting
+        setNbrOrders(orderToDisplay.length);
+
+        // Fetch food details for each order to display
+
+        const fetchFoodDetailsPromises = orderToDisplay.slice(0, 5).map((order) => {
+          return fetch(
+            `http://${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/${process.env.REACT_APP_NBR_RESTAURANT}/orders/${order.id}?forKDS=true`
+          )
             .then((response) => response.json())
             .then((data) => {
-              const orderDateObj = new Date(data.date);
+              const orderDateObj = new Date(order.date);
               const orderDate = {
                 hours: String(orderDateObj.getHours()).padStart(2, "0"),
                 minutes: String(orderDateObj.getMinutes()).padStart(2, "0"),
                 seconds: String(orderDateObj.getSeconds()).padStart(2, "0"),
               };
-              return { ...data, orderDate };
+              ordersData.find((singleOrder) => singleOrder.id === order.id).food_ordered = data.food_ordered;
+              ordersData.find((singleOrder) => singleOrder.id === order.id).orderDate = orderDate;
             });
         });
 
-        Promise.all(fetchOrderDetailsPromises).then((orderDetailsArray) => {
-          const ordersLineComponents = orderDetailsArray.map((orderDetails) => ({
-            component: (
-              <SingleOrderDisplay
-                key={orderDetails.number}
-                orderDetails={orderDetails}
-                span={getNbrColumns(orderDetails)}
-                updateOrders={fetchOrders}
-              />
-            ),
-            nbrCol: getNbrColumns(orderDetails),
-          }));
+        Promise.all(fetchFoodDetailsPromises).then(() => {
+          const ordersLineComponents = orderToDisplay.slice(0, 5).map(
+            (orderDetails) => ({
+              component: (
+                <SingleOrderDisplay
+                  key={orderDetails.number}
+                  orderDetails={orderDetails}
+                  span={getNbrColumns(orderDetails)}
+                />
+              ),
+              nbrCol: getNbrColumns(orderDetails),
+            })
+          );
 
           let ordersLine1 = [];
           let currentLine1Cols = 0;
